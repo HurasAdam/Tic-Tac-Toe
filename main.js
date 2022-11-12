3848240cb01fa937b1832030c1b704f20796caea
HurasAdam <huradam@wp.pl>
2022-11-12 13:57:30 +0100

    script rework

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const PLAYER1 = "✖";
 const PLAYER2 = "◯";
 let round = 1;
 
-const PL1 = [0,1,2];
+const PL1 = [1, 2, 3];
 const PL2 = [];
 const board__fields = document.querySelectorAll(".board__field");
 const resetButton = document.querySelector(".reset");
@@ -25,7 +25,12 @@ board__fields.forEach((div, index) => {
   });
 });
 
-const winningConditions = [ [0, 1, 2],[2, 1, 0],[3, 4, 5],[5, 4, 3],[6, 7, 8],
+const winningConditions = [
+  [0, 1, 2],
+  [2, 1, 0],
+  [3, 4, 5],
+  [5, 4, 3],
+  [6, 7, 8],
   [8, 7, 6],
   [0, 4, 8],
   [8, 4, 0],
@@ -39,16 +44,14 @@ const winningConditions = [ [0, 1, 2],[2, 1, 0],[3, 4, 5],[5, 4, 3],[6, 7, 8],
   [8, 5, 2],
 ];
 
-function check(arr){
-  let result = arr.every((element)=>{
-    return(element===winningConditions[element])
-  })
-  console.log(element)
+function check(arr) {
+  let result = arr.every((element) => {
+    return element === winningConditions[element];
+  });
+  console.log(element);
 }
 
 const resetGame = () => {
-
-  
   PL1.splice(0, PL1.length);
   PL2.splice(0, PL2.length);
 
@@ -58,62 +61,3 @@ const resetGame = () => {
 };
 
 resetButton.addEventListener("click", resetGame);
-
-
-
-
-function doesIcludes(){
-// const a= PL1.toString();
-  winningConditions.forEach(function(item){
-
-item.filter(function(element){
-  element.includes(PL1);
-})
-  
-  })
-
-
-}
-
-
-
-
-  // const a= PL1.toString();
-    
-  
-  // const filter=winningConditions.filter(function(element,index,array){
-  //    array[index]===PL1;
-    
-  // })
-  // console.log(filter)
-  
-  const arr1d=[].concat(...winningConditions)
-  
-    const mapowanie = arr1d.map(function(element,index,array){
- return array[index];
-  
-})
-    
-    console.log(mapowanie)
-  
-  const filterNewMapedArray=mapowanie.filter(function(element){
-    
-  })
-  console.log(filterNewMapedArray);
-
-
-
-
-// const winningConditions = [ [0, 1, 2],[2, 1, 0],[3, 4, 5],[5, 4, 3],[6, 7, 8],
-//   [8, 7, 6],
-//   [0, 4, 8],
-//   [8, 4, 0],
-//   [6, 4, 2],
-//   [2, 4, 6],
-//   [0, 3, 6],
-//   [6, 3, 0],
-//   [1, 4, 7],
-//   [7, 4, 1],
-//   [2, 5, 8],
-//   [8, 5, 2],
-// ];
