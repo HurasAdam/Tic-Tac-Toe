a6eb135cb5973c06c98e2bea040adc7dc77df284
HurasAdam <huradam@wp.pl>
2022-11-14 02:19:35 +0100

    update check/display winner functions

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
 const PLAYER1 = "✖";
 const PLAYER2 = "◯";
 let round = 1;
-
-const PL1 = [];
-const PL2 = [];
-const board__fields = document.querySelectorAll(".board__field");
-const resetButton = document.querySelector(".reset");
-board__fields.forEach((div, index) => {
-  div.addEventListener("click", (event) => {
-    const item = event.target;
-
-    if (item.innerHTML !== "") {
-      return;
-    }
-
-    if (round % 2 === 0) {
-      item.innerHTML = PLAYER1;
-      PL1.push(index);
-    } else if (round % 2 !== 0) {
-      item.innerHTML = PLAYER2;
-      PL2.push(index);
-    }
-    round++;
-  });
-});
-
 const winningConditions = [
   [0, 1, 2],
   [2, 1, 0],
@@ -44,11 +20,88 @@ const winningConditions = [
   [8, 5, 2],
 ];
 
-function isWinning(winningCombinations, player) {
-  return winningCombinations.some((combination) => {
-    return combination.every((cell) => player.includes(cell));
+const PL1 = [];
+const PL2 = [];
+const board__fields = document.querySelectorAll(".board__field");
+const resetButton = document.querySelector(".reset");
+
+window.onload= setGame();
+
+function setGame(){
+board__fields.forEach((div, index) => {
+  div.addEventListener("click", (event) => {
+    const item = event.target;
+
+    if (item.innerHTML !== "") {
+      return;
+    }
+
+    if (round % 2 === 0) {
+      item.innerHTML = PLAYER1;
+      PL1.push(index);
+    } else if (round % 2 !== 0) {
+      item.innerHTML = PLAYER2;
+      PL2.push(index);
+    }
+    round++;
+    isWinning();
+    
   });
+  displayWinner();
+});
 }
+
+
+
+
+function isWinning() {
+  let winner=''
+ 
+
+  const p1Choices=winningConditions.some((combination) => {
+   
+    return combination.every((cell) => PL1.includes(cell));
+    
+  });
+  const p2Choices=winningConditions.some((combination) => {
+   
+    return combination.every((cell) => PL2.includes(cell));
+    
+  
+  });
+  if(p1Choices===true){
+    winner='Player 1'
+    
+  }
+  else if(p2Choices===true){
+    winner='Player 2'
+  }
+
+  if(winner==='Player 1'){
+    return winner;
+    
+  }
+  else if(winner==='Player 2'){
+  return winner;
+  }
+  
+ 
+ 
+}
+
+
+
+
+
+function displayWinner(){
+const wrapper= document.querySelector('.wrapper');
+const resultBox=document.createElement('div');
+resultBox.classList.add('resultBox')
+wrapper.appendChild(resultBox);
+resultBox.innerHTML= isWinning();
+return;
+}
+
 
 const resetGame = () => {
   PL1.splice(0, PL1.length);
