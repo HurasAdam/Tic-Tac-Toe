e51dddc4d5df0fb3ea8986dacc37b3fadf15aef3
HurasAdam <huradam@wp.pl>
2022-11-16 03:16:19 +0100

    update

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const PLAYER1 = "✖";
 const PLAYER2 = "◯";
+let PLAYER1SCORE = 0;
+let PLAYER2SCORE = 0;
 let round = 1;
 const winningConditions = [
   [0, 1, 2],
@@ -28,9 +30,15 @@ const winnerPopup = document.querySelector(".winnerPopup");
 const wrapper = document.querySelector(".wrapper");
 const resetPopupButton = document.querySelector(".resetPopup");
 const announceWinnerSpan = document.querySelector(".announceWinner");
+const playerOneTurn = document.querySelector(".player1Turn");
+const playerTwoTurn = document.querySelector(".player2Turn");
 window.onload = setGame();
 
 function setGame() {
+  const playerOneValue = document.querySelector(".playerOneValue");
+  const playerTwoValue = document.querySelector(".playerTwoValue");
+  playerOneValue.innerHTML = PLAYER1SCORE;
+  playerTwoValue.innerHTML = PLAYER2SCORE;
   board__fields.forEach((div, index) => {
     div.addEventListener("click", (event) => {
       const item = event.target;
@@ -41,9 +49,13 @@ function setGame() {
         }
 
         if (round % 2 === 0) {
+          playerOneTurn.classList.remove("active");
+          playerTwoTurn.classList.add("active");
           item.innerHTML = PLAYER1;
           PL1.push(index);
         } else if (round % 2 !== 0) {
+          playerTwoTurn.classList.remove("active");
+          playerOneTurn.classList.add("active");
           item.innerHTML = PLAYER2;
           PL2.push(index);
         }
@@ -68,22 +80,24 @@ function isWinning() {
     return combination.every((cell) => PL2.includes(cell));
   });
   if (p1Choices === true) {
-    winner = "Player 1";
+    winner = "Player 1 has won";
+    PLAYER1SCORE++;
   } else if (p2Choices === true) {
-    winner = "Player 2";
+    winner = "Player 2 has won";
+    PLAYER2SCORE++;
   } else if (PL1.length === 3 && PL2.length === 3 && winner === undefined) {
-    console.log("DRAW");
+    winner = "DRAW";
   }
 
-  if (winner === "Player 1") {
-  } else if (winner === "Player 2") {
+  if (winner === "Player 1 has won") {
+  } else if (winner === "Player 2 has won") {
   }
   return winner;
 }
 
 function displayWinner() {
   if (isWinning() !== undefined) {
-    announceWinnerSpan.innerHTML = `${isWinning()} has won`;
+    announceWinnerSpan.innerHTML = isWinning();
     winnerPopup.classList.add("active");
     wrapper.classList.add("active");
   } else {
@@ -94,7 +108,8 @@ function displayWinner() {
 const resetGame = () => {
   PL1.splice(0, PL1.length);
   PL2.splice(0, PL2.length);
-
+  playerOneTurn.classList.remove("active");
+  playerTwoTurn.classList.remove("active");
   board__fields.forEach((div) => {
     div.innerHTML = "";
   });
