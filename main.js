7798b053747de88cf236a32207bd17dded6266e5
typeness <tangens270@gmail.com>
2022-12-01 20:31:45 +0100

    Object oriented code version

--- main.js
+++ main.js
@@ -1,133 +1,159 @@
-const PLAYER1 = "✖";
-const PLAYER2 = "◯";
-let PLAYER1SCORE = 0;
-let PLAYER2SCORE = 0;
-let round = 1;
+class Player {
+    constructor(sign) {
+        this.sign = sign
+        this.score = 0
+    }
+}
+
 const winningConditions = [
-  [0, 1, 2],
-  [2, 1, 0],
-  [3, 4, 5],
-  [5, 4, 3],
-  [6, 7, 8],
-  [8, 7, 6],
-  [0, 4, 8],
-  [8, 4, 0],
-  [6, 4, 2],
-  [2, 4, 6],
-  [0, 3, 6],
-  [6, 3, 0],
-  [1, 4, 7],
-  [7, 4, 1],
-  [2, 5, 8],
-  [8, 5, 2],
+    [0, 1, 2],
+    [2, 1, 0],
+    [3, 4, 5],
+    [5, 4, 3],
+    [6, 7, 8],
+    [8, 7, 6],
+    [0, 4, 8],
+    [8, 4, 0],
+    [6, 4, 2],
+    [2, 4, 6],
+    [0, 3, 6],
+    [6, 3, 0],
+    [1, 4, 7],
+    [7, 4, 1],
+    [2, 5, 8],
+    [8, 5, 2],
 ];
 
-const PL1 = [];
-const PL2 = [];
+class GameState {
+    constructor() {
+        this.playerO = new Player("◯", 0)
+        this.playerX = new Player("✖", 0)
+        this.resetGame()
+    }
+
+    resetGame() {
+        this.board = Array.from(' '.repeat(9))
+        this.playerXPositions = []
+        this.playerOPositions = []
+        this.currentPlayer = this.getRandomPlayer()
+        this.winner = null
+    }
+
+    getRandomPlayer() {
+        if (Math.round(Math.random()) % 2 === 0) {
+            return this.playerO
+        } else {
+            return this.playerX
+        }
+    }
+
+    isDraw() {
+        return !this.board.includes(' ')
+    }
+
+    isGameOver() {
+        return this.isDraw() || this.winner != null
+    }
+
+    checkWinner() {
+        function isWinner(playerPositions) {
+            return winningConditions.some((combination) => {
+                return combination.every((cell) => playerPositions.includes(cell));
+            });
+        }
+
+        if (isWinner(this.playerXPositions)) {
+            this.winner = this.playerX
+            this.playerX.score++
+        } else if (isWinner(this.playerOPositions)) {
+            this.winner = this.playerO
+            this.playerO.score++
+        }
+    }
+
+    canMakeMove(index) {
+        return !this.isGameOver() && this.board[index] === ' '
+    }
+
+    makeNextTurn(index) {
+        if (this.currentPlayer.sign === '◯') {
+            this.playerOPositions.push(index)
+            this.currentPlayer = this.playerX
+        } else {
+            this.playerXPositions.push(index)
+            this.currentPlayer = this.playerO
+        }
+    }
+
+    makeMove(index) {
+        if (this.canMakeMove(index)) {
+            this.board[index] = this.currentPlayer.sign
+            this.makeNextTurn(index)
+        }
+        this.checkWinner()
+    }
+}
+
+game = new GameState()
+
+function onPlayerClick(index) {
+    game.makeMove(index)
+    display()
+}
+
+function onResetGame() {
+    game.resetGame()
+    display()
+}
+
+function onResetPoints() {
+    game = new GameState()
+    display()
+}
+
+function display() {
+    board__fields.forEach((div, index) => {
+        div.innerHTML = game.board[index]
+    });
+    playerOneValue.innerHTML = game.playerX.score;
+    playerTwoValue.innerHTML = game.playerO.score;
+    if (game.currentPlayer === game.playerX) {
+        playerXTurn.classList.remove("active");
+        playerOTurn.classList.add("active");
+    } else {
+        playerXTurn.classList.add("active");
+        playerOTurn.classList.remove("active");
+    }
+    if (game.isGameOver()) {
+        announceWinnerSpan.innerHTML = game.winner !== null ? game.winner.sign : 'Draw'
+        winnerPopup.classList.add("active");
+        wrapper.classList.add("active");
+    } else {
+        announceWinnerSpan.innerHTML = ''
+        winnerPopup.classList.remove("active");
+        wrapper.classList.remove("active");
+    }
+}
+
 const board__fields = document.querySelectorAll(".board__field");
 const resetButton = document.querySelector(".reset");
 const winnerPopup = document.querySelector(".winnerPopup");
 const wrapper = document.querySelector(".wrapper");
 const resetPopupButton = document.querySelector(".resetPopup");
 const announceWinnerSpan = document.querySelector(".announceWinner");
-const playerOneTurn = document.querySelector(".player1Turn");
-const playerTwoTurn = document.querySelector(".player2Turn");
+const playerOTurn = document.querySelector(".player1Turn");
+const playerXTurn = document.querySelector(".player2Turn");
 const playerOneValue = document.querySelector(".playerOneValue");
 const playerTwoValue = document.querySelector(".playerTwoValue");
-window.onload = setGame();
 
-function setGame() {
-  playerOneValue.innerHTML = PLAYER1SCORE;
-  playerTwoValue.innerHTML = PLAYER2SCORE;
-  board__fields.forEach((div, index) => {
-    div.addEventListener("click", (event) => {
-      const item = event.target;
 
-      if (item.innerHTML === "") {
-        if (PL1.length === 3 && PL2.length === 3) {
-          return;
-        }
+resetPopupButton.addEventListener("click", onResetGame);
+resetButton.addEventListener("click", onResetPoints);
 
-        if (round % 2 === 0) {
-          playerTwoTurn.classList.remove("active");
-          playerOneTurn.classList.add("active");
-          item.innerHTML = PLAYER2;
-          PL2.push(index);
-        } else if (round % 2 !== 0) {
-          playerOneTurn.classList.remove("active");
-          playerTwoTurn.classList.add("active");
-          item.innerHTML = PLAYER1;
-          PL1.push(index);
-        }
-      } else {
-        return;
-      }
-
-      round++;
-      isWinning();
-      displayWinner();
+board__fields.forEach((div, index) => {
+    div.addEventListener("click", (_) => {
+        onPlayerClick(index)
     });
-  });
-}
+});
 
-function isWinning() {
-  let winner;
-
-  const p1Choices = winningConditions.some((combination) => {
-    return combination.every((cell) => PL1.includes(cell));
-  });
-  const p2Choices = winningConditions.some((combination) => {
-    return combination.every((cell) => PL2.includes(cell));
-  });
-  if (p1Choices === true) {
-    winner = "Player 1 has won";
-    PLAYER1SCORE++;
-  } else if (p2Choices === true) {
-    winner = "Player 2 has won";
-    PLAYER2SCORE++;
-  } else if (PL1.length === 3 && PL2.length === 3 && winner === undefined) {
-    winner = "DRAW";
-  }
-
-  if (winner === "Player 1 has won") {
-  } else if (winner === "Player 2 has won") {
-  }
-  return winner;
-}
-
-function displayWinner() {
-  if (isWinning() !== undefined) {
-    announceWinnerSpan.innerHTML = isWinning();
-    winnerPopup.classList.add("active");
-    wrapper.classList.add("active");
-    playerOneValue.innerHTML = PLAYER1SCORE;
-    playerTwoValue.innerHTML = PLAYER2SCORE;
-  } else {
-    return;
-  }
-}
-
-const resetGame = () => {
-  PL1.splice(0, PL1.length);
-  PL2.splice(0, PL2.length);
-  playerOneTurn.classList.remove("active");
-  playerTwoTurn.classList.remove("active");
-  winnerPopup.classList.remove("active");
-  wrapper.classList.remove("active");
-
-  board__fields.forEach((div) => {
-    div.innerHTML = "";
-    round = 1;
-  });
-};
-
-resetPopupButton.addEventListener("click", resetGame);
-resetButton.addEventListener("click", resetPoints);
-
-function resetPoints() {
-  PLAYER1SCORE = 0;
-  PLAYER2SCORE = 0;
-  playerOneValue.innerHTML = 0;
-  playerTwoValue.innerHTML = 0;
-  resetGame();
-}
+display()
